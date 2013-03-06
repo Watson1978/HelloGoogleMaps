a0fee2e6d5bc5faed4e3057cb46b8d5d10903ad3
Watson <watson1978@gmail.com>
2013-03-07 00:55:19 +0900

    avoid build error because Google Maps SDK does not have some symbols for device
    see https://code.google.com/p/gmaps-api-issues/issues/detail?id=5018

---
+++ vendor/fix_google_sdk_arm_issue/fix_google_sdk_arm_issue/fix_google_sdk_arm_issue.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+@interface fix_google_sdk_arm_issue : NSObject
+
+@end
---
+++ vendor/fix_google_sdk_arm_issue/fix_google_sdk_arm_issue/fix_google_sdk_arm_issue.m
@@ -0,0 +1,8 @@
+#import "fix_google_sdk_arm_issue.h"
+
+@implementation fix_google_sdk_arm_issue
+
+@end
+
+void WebPInitPremultiplyNEON(void) {}
+void WebPInitUpsamplersNEON(void) {}
